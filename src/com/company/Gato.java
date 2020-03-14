1405fb314c4de414404c5e7f90e21b1d0d010e76
alan <alanspina@gmail.com>
2020-03-14 15:55:48 -0300

    Cree todas las responsabilidades

--- src/com/company/Gato.java
+++ src/com/company/Gato.java
@@ -3,10 +3,33 @@ package com.company;
 import java.util.List;
 
 public class Gato {
-private String nombre;
-private Integer energia;
-private Integer velocidad;
-private List<Raton> listaDeRatones;
+    private String nombre;
+    private Double energia;
+    private Double velocidad;
+
+    public Gato(String nombre, Integer energia, Integer velocidad) {
+        this.nombre = nombre;
+        this.energia = 0.0;
+        this.velocidad = 5.0;
+    }
+
+    public void comerRaton(Raton unRaton) {
+        this.energia = this.energia + unRaton.getPeso();
+        System.out.println("me comi un raton mi nueva energia es " + energia);
+    }
+
+    public void comerRaton(List<Raton> ratones) {
+        for (Raton unRaton : ratones) {
+            comerRaton(unRaton);
+        }
+    }
+
+    public Double correr(Integer cantidadDeMetros) {
+        this.energia = this.energia - 0.5*cantidadDeMetros;
+        return cantidadDeMetros/this.velocidad;
+    }
+
+
 
 
 }
--- src/com/company/Raton.java
+++ src/com/company/Raton.java
@@ -4,4 +4,16 @@ public class Raton {
     private String nombre;
     private Integer peso;
 
+    public Raton(String nombre, Integer peso) {
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
 }
