0c08c040d1d22cda27e5e6e674a19bd31cea21d0
alan <alanspina@gmail.com>
2020-03-14 15:39:38 -0300

    Initial commit

---
+++ src/com/company/Gato.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.List;
+
+public class Gato {
+private String nombre;
+private Integer energia;
+private Integer velocidad;
+private List<Raton> listaDeRatones;
+
+
+}
---
+++ src/com/company/Raton.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Raton {
+    private String nombre;
+    private Integer peso;
+
+}
